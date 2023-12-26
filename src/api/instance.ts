067fc6019f3c23e8275b0bd624bddca9048f2185
liu-purnomo <kontak.liu@gmail.com>
2023-12-26 15:43:36 +0700

    feat : adding constant data

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { baseURL } from "../../constants";
+import { baseURL } from "../../notess";
 
 export const instance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL || baseURL,
